0377f471e3832acd4496a50b0abbb1fa071a2a2b
wiktor Kęska <wkeska@silenteight.com>
2021-11-03 23:46:49 +0100

    fixed deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 docker build -t grzybek/translatoinator-web .
-ssh docker stop $(docker ps -q --filter ancestor=grzybek/translatoinator-web )
+ssh grzybek docker stop $(docker ps -q --filter ancestor=grzybek/translatoinator-web )
 docker save grzybek/translatoinator-web | bzip2 | pv | ssh grzybek docker load
 ssh grzybek docker run -p 5000:5000 --net=host -d grzybek/translatoinator-web
