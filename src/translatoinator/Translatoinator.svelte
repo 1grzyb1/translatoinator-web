b558fb7585ee51193244420713ea082042416efe
wiktor Kęska <wkeska@silenteight.com>
2021-11-03 23:08:23 +0100

    error handling

--- src/translatoinator/Translatoinator.svelte
+++ src/translatoinator/Translatoinator.svelte
@@ -14,15 +14,19 @@
                     on:click={translate}>
                 Translate
             </button>
-            {#if translated != null}
-                <div class="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3" role="alert">
-                    <p class="font-bold">Translated text</p>
-                    <p class="text-sm">{translated.transformedWord}</p>
-                    <p></p>
-                    <p class="font-bold">Translation path</p>
-                    {#each translated.languages as language}
-                        <p class="text-sm">{language}</p>
-                    {/each}
+            {#if (failed)}
+                <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+                    <strong class="font-bold">OMG!</strong>
+                    <span class="block sm:inline">Something unimaginable had happened.</span>
+                    <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                        <svg class="fill-current h-6 w-6 text-red-500" role="button"
+                             xmlns="http://www.w3.org/2000/svg"
+                             viewBox="0 0 20 20"
+                             on:click={closeAlert}>
+                            <title>Close</title>
+                            <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/>
+                        </svg>
+                    </span>
                 </div>
             {/if}
         </div>
@@ -38,7 +42,7 @@
 
     export let baseUrl
 
-    let text;
+    let text = '';
     let times = 10;
 
     let translated
@@ -46,6 +50,7 @@
     let language = {}
 
     let loading = false
+    let failed = false
 
     onMount(async () => {
         const res = await fetch(baseUrl + '/randomTranslate');
@@ -65,11 +70,16 @@
             body: JSON.stringify({
                 word: text,
                 repeat: times,
-                inputLanguageCode: language.code
+                // inputLanguageCode: language.code
             })
         })
         translated = await res.json()
+        failed = res.status !== 200
         loading = false
         console.log(translated)
+    }
+
+    function closeAlert() {
+        failed = false
     }
 </script>
