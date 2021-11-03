d18f312269d3d228a5a4e7cc4033c5c40b07a6f7
wiktor Kęska <wkeska@silenteight.com>
2021-11-03 22:50:08 +0100

    fixed default language

---
+++ src/translatoinator/Combobox.svelte
@@ -0,0 +1,23 @@
+<main>
+    <link href="https://cdn.jsdelivr.net/npm/@tailwindcss/custom-forms@0.2.1/dist/custom-forms.css"
+            rel="stylesheet"
+    />
+
+    <label class="block text-gray-700 text-sm font-bold mb-2">
+        <span class="text-gray-700">Select default language</span>
+        <select class="form-select block w-full mt-1" bind:value={selected}>
+            {#each list as item}
+                {#if item.code === 'en'}
+                    <option selected="selected" value="{item}" class="text-gray-700">{item.name}</option>
+                {:else}
+                    <option value="{item}" class="text-gray-700">{item.name}</option>
+                {/if}
+            {/each}
+        </select>
+    </label>
+</main>
+
+<script>
+    export let list
+    export let selected
+</script>
--- src/translatoinator/Translatoinator.svelte
+++ src/translatoinator/Translatoinator.svelte
@@ -1,18 +1,33 @@
 <main>
     <Navbar/>
     <div class="flex justify-center h-auto p-20">
-        <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-1/2 space-y-5">
+        <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 space-y-5 md:w-1/2 sm:w-full">
+            <Combobox list="{languages}"
+                      bind:selected={language}/>
             <TextBox label="Phrase to translate"
                      bind:value="{text}"/>
             <NumberInput label="Times to repeat"
                          bind:value={times}/>
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full" on:click={translate}>
-                Translate
-            </button>
-            {#if response != null}
+            {#if (loading === false)}
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
+                        on:click={translate}>
+                    Translate
+                </button>
+                {:else}
+                <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded opacity-50 cursor-not-allowed w-full"
+                        on:click={translate}>
+                    Translate
+                </button>
+                {/if}
+            {#if translated != null}
                 <div class="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3" role="alert">
                     <p class="font-bold">Translated text</p>
-                    <p class="text-sm">{response}</p>
+                    <p class="text-sm">{translated.transformedWord}</p>
+                    <p></p>
+                    <p class="font-bold">Translation path</p>
+                    {#each translated.languages as language}
+                        <p class="text-sm">{language}</p>
+                    {/each}
                 </div>
             {/if}
         </div>
@@ -23,23 +38,43 @@
     import Navbar from "./Navbar.svelte";
     import TextBox from "./TextInput.svelte";
     import NumberInput from "./NumberInput.svelte";
+    import { onMount } from 'svelte';
+    import Combobox from "./Combobox.svelte";
 
     export let baseUrl
 
     let text;
-    let times;
+    let times = 10;
 
-    let response
+    let translated
+    let languages = {}
+    let language = {}
+
+    let loading = false
+
+    onMount(async () => {
+        const res = await fetch(baseUrl + '/randomTranslate');
+        languages = await res.json();
+        language = languages.find(element => element.code === 'en')
+        console.log(languages)
+    });
 
     async function translate() {
+        console.log(language)
+        loading = true
+        if (times > 10)
+            return
         const res = await fetch(baseUrl + '/randomTranslate', {
             method: 'POST',
-            headers: { "Content-Type": "application/json" },
+            headers: {"Content-Type": "application/json"},
             body: JSON.stringify({
                 word: text,
-                repeat: times
+                repeat: times,
+                inputLanguageCode: language.code
             })
         })
-        response = await res.text()
+        translated = await res.json()
+        loading = false
+        console.log(translated)
     }
 </script>
